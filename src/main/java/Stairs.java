5c7eca1c8510446e91c2053f2e90dade4011bc64
Name Surname <zxckvzxc@gmail.com>
2017-06-24 20:37:05 +0300

    #5 refactor

--- src/main/java/Stairs.java
+++ src/main/java/Stairs.java
@@ -1,29 +1,24 @@
 public class Stairs {
     private static long L[][] = new long[501][501];
 
-    public long calculateStairs(int cubes) {
+    private static long solve(int i, int j) {
+        if(i == 0) return 1;
+        if(j == 0) return 0;
+        if(j > i) return L[i][i];
+        return L[i-j][j-1] + L[i][j-1];
+    }
+
+    public static long calculateStairs(int cubes) {
         for(int i=0; i<501; i++) {
             for(int j=0; j<501; j++) {
-                if(i == 0) {
-                    L[i][j] = 1;
-                    continue;
-                }
-                if(j == 0) {
-                    L[i][j] = 0;
-                    continue;
-                }
-                if(j > i) {
-                    L[i][j] = L[i][i];
-                    continue;
-                }
-                L[i][j] = L[i-j][j-1] + L[i][j-1];
+                L[i][j] = solve(i,j);
             }
         }
-
         int N = cubes;
-        if (0 == N)
-            return 0L;
-        else
-            return L[N][N-1];
+        return solve(N, N) - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateStairs(500));
     }
 }
