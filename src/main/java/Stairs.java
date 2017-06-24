29d91de12b69cd76897a6d44ce56ee598d835ade
Name Surname <zxckvzxc@gmail.com>
2017-06-24 20:35:06 +0300

    #5 pass test

--- src/main/java/Stairs.java
+++ src/main/java/Stairs.java
@@ -1,10 +1,29 @@
 public class Stairs {
+    private static long L[][] = new long[501][501];
+
     public long calculateStairs(int cubes) {
-        if (3 == cubes || 4 == cubes)
-            return 1L;
-        else if (5 == cubes)
-            return 2L;
-        else
+        for(int i=0; i<501; i++) {
+            for(int j=0; j<501; j++) {
+                if(i == 0) {
+                    L[i][j] = 1;
+                    continue;
+                }
+                if(j == 0) {
+                    L[i][j] = 0;
+                    continue;
+                }
+                if(j > i) {
+                    L[i][j] = L[i][i];
+                    continue;
+                }
+                L[i][j] = L[i-j][j-1] + L[i][j-1];
+            }
+        }
+
+        int N = cubes;
+        if (0 == N)
             return 0L;
+        else
+            return L[N][N-1];
     }
 }
